bb26c694cec0bd08a36ac5ee23d37f29d762021c
fullstackren <liferzy@163.com>
2025-01-22 14:43:03 +0800

    feat:Python

--- docs/.vitepress/configs/sidebar.ts
+++ docs/.vitepress/configs/sidebar.ts
@@ -6,6 +6,11 @@ export const sidebar: DefaultTheme.Config['sidebar'] = {
       text: '文档指南',
       items: [
         { text: '内心要说的话', link: '/guide/index' },
+        { text: '基础', collapsed: false, 
+          items: [
+            { text: '数据类型', link: '/guide/data-types' },
+          ]
+        }
       ]
     }
   ],
